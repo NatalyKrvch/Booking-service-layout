eebd5048fcce31c3e378ba050028a8cbbfab79fe
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2024-08-20 23:48:46 +0300

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 import { loadCards } from './loadCard.js';
-import '../css/styles.css';
-import '../css/keyframes.css';
-import '../css/cards.css';
-import '../css/constants.css';
-import '../css/filters.css';
-import '../css/header.css';
-import '../css/media-queries.css';
-import '../css/scrollbar.css';
 
 document.addEventListener('DOMContentLoaded', loadCards);
